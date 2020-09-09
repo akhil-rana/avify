128a769e300ce06d16344255da8f2526db174e2d
akhil-rana <akhilrana0001@gmail.com>
2020-09-09 22:07:52 +0530

    live changes and somethings

--- src/App.js
+++ src/App.js
@@ -1,23 +1,49 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import { makeStyles } from '@material-ui/core/styles';
+import Button from '@material-ui/core/Button';
+import IconButton from '@material-ui/core/IconButton';
+import PhotoCamera from '@material-ui/icons/PhotoCamera';
+
+const useStyles = makeStyles((theme) => ({
+  root: {
+    '& > *': {
+      margin: theme.spacing(1),
+    },
+  },
+  input: {
+    display: 'none',
+  },
+}));
+
+function UploadButton() {
+  const classes = useStyles();
+
+  return (
+    <div className={classes.root}>
+      <input
+        accept='image/*'
+        className={classes.input}
+        id='contained-button-file'
+        multiple
+        type='file'
+      />
+      <label htmlFor='contained-button-file'>
+        <Button variant='contained' color='primary' component='span'>
+          Upload
+        </Button>
+      </label>
+    </div>
+  );
+}
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
+    <div className='App'>
+      <header className='App-header'>
+        <p>Compress images to the new AVIF codec</p>
+        <br />
+        {UploadButton()}
       </header>
     </div>
   );
--- src/electron-starter.js
+++ src/electron-starter.js
@@ -3,19 +3,23 @@ const path = require('path');
 const url = require('url');
 const app = electron.app;
 const BrowserWindow = electron.BrowserWindow;
-const startUrl = process.env.ELECTRON_START_URL || url.format({
+const startUrl =
+  process.env.ELECTRON_START_URL ||
+  url.format({
     pathname: path.join(__dirname, '/../build/index.html'),
     protocol: 'file:',
-    slashes: true
-});
+    slashes: true,
+  });
+
 let mainWindow;
 function createWindow() {
   mainWindow = new BrowserWindow({
     width: 800,
     height: 600,
-    webPreferences: {
+    webPreferences: { 
       nodeIntegration: true,
     },
+    icon: './assets/favicon-32x32.png', 
   });
   mainWindow.loadURL(startUrl);
   // mainWindow.webContents.openDevTools();
